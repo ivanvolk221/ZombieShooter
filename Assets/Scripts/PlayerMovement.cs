6a30c17c09e2afaeddea8558ca8432f13f426927
ivanvolk221 <87665909+ivanvolk221@users.noreply.github.com>
2022-09-26 07:46:29 +0300

    2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,32 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float Speed;
-    //[SerializeField] float MovementSpeed;
-    private static float vertical, horizontal;
+    [SerializeField] private float _speed;
+
+    //private static float vertical, horizontal;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        transform.Translate(horizontal, vertical, 0);
+        //horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        //vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        //transform.Translate(horizontal, vertical, 0);
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * _speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);    
     }
 }
